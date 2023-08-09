a1512b9b8bd5f274fbf9661b427fdef2f0b1d07b
Shut Kuva <zekazyza763@gmail.com>
2023-08-09 15:04:49 +0300

    Adding abstractions

---
+++ DAL.Abstractions/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace DAL.Abstractions.Repository
+{
+    public interface IRepository<TEntity, TId, TPredicate>
+    {
+        Task<TEntity> GetEntityByIdAsync(TId id);
+        Task<IEnumerable<TEntity>> GetEntitiesByPredicateAsync(TPredicate predicate);
+        Task<bool> CreateEntityAsync(TEntity entity);
+        Task<bool> UpdateEntityAsync(TEntity entity);
+        Task<bool> DeleteEntityAsync(TId id);
+    }
+}
---
+++ DAL.Abstractions/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Abstractions.Repository;
+
+namespace DAL.Abstractions.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Task<IRepository<TEntity, TId, TPredicate>> GetRepositoryAsync<TEntity, TId, TPredicate>();
+        Task CommitAsync();
+    }
+}
